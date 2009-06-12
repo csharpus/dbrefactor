a51dd975571cfe2c1ee0904f4ba1ec018b324287
egor.rubansky <egor.rubansky@0d2ccd80-b940-0410-ba80-c9055f8e4fa4>
2009-06-12 23:14:52 +0000

    added base table class

---
+++ trunk/Source/Core/Table/Table.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DbRefactor.Providers;
+
+namespace DbRefactor
+{
+	public abstract class Table
+	{
+		protected IDatabaseEnvironment databaseEnvironment;
+		public string TableName { get; set; }
+
+		protected Table(IDatabaseEnvironment environment)
+		{
+			databaseEnvironment = environment;
+		}
+
+	}
+}
